de947f7993ec7c2d81c492dd495272b19474b734
sumanth070502 <sumanthkumar3.p@gmail.com>
2022-10-21 22:07:29 +0530

    first Assignment

---
+++ Pattern1.java
@@ -0,0 +1,38 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int n=15;
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					if(i==0 || i==n-1 || j==(n-1)/2) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) {
+				if(j==0 || j==n-1 || j==i) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) {
+				if(j==0 || i==0 || i==n-1 || i==(n-1)/2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+			}
+	}
+}
+
