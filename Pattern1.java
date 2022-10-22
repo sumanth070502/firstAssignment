3b4f388a8879534f3f38ad4ca72e23ccf645e02f
sumanth070502 <sumanthkumar3.p@gmail.com>
2022-10-22 20:50:50 +0530

    First Assignment

--- Pattern1.java
+++ Pattern1.java
@@ -31,6 +31,42 @@ public class Pattern1 {
 					System.out.print(" ");
 				}
 			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) {
+				if(j==0 && i<n-1 || i==n-1  || j==n-1 && i<n-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) {
+				if(i == 0 && j>0 && j<n-1 || j == 0 || i == n / 2 && j<n-1 || i == j && i > n / 2 || j == n - 1 && i>0 && i < n / 2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) {
+				if(i == 0 && j>0 && j<n-1|| j == 0 && i>0 && i<n-1  || i==n-1 && j>0 && j<n-1 || j==n-1 && i>0 && i<n-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) {
+				if(j==0 || j==n-1 || j==i) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 			}
 	}
